512b1e2334c5213e295dc787304bef289f0869cc
zzbased <zerobased@126.com>
2015-08-23 13:05:07 +0800

    conversion

--- other/conversion.sh
+++ other/conversion.sh
@@ -9,4 +9,7 @@ cat head.txt temp.md > ../_posts/2015-03-25-机器学习基石学习笔记.md
 sed  's/!\[\](\(.*\)/![](https:\/\/raw.githubusercontent.com\/zzbased\/zzbased.github.com\/master\/other\/mlfoundation_learn\/\1/g' mlfoundation_learn/机器学习技法学习笔记.md > temp.md
 cat head.txt temp.md > ../_posts/2015-03-26-机器学习技法学习笔记.md
 
+sed  's/!\[\](\(.*\)/![](https:\/\/raw.githubusercontent.com\/zzbased\/zzbased.github.com\/master\/other\/leetcode\/\1/g' leetcode/leetcode.md > temp.md
+cat head.txt temp.md > ../_posts/2015-05-21-leetcode刷题小结.md
+
 rm temp.md
